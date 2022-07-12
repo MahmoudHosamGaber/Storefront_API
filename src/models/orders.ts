18b5657a0e02de8f713aaccf9f9bf4d4a0f84e5e
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-07-13 00:32:44 +0200

    test: Add endpoint tests for the order spec

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -8,7 +8,7 @@ export type Order = {
 };
 
 export class OrderStore {
-  async getUserOrder(user_id: number | string): Promise<Product[]> {
+  async getUserOrder(user_id: string): Promise<Product[]> {
     const conn = await Client.connect();
     const getUserOrder = `SELECT p.name, p.price, sum(op.quantity) FROM
     orders as o INNER JOIN order_products as op
@@ -17,9 +17,7 @@ export class OrderStore {
     ON op.order_id = p.id
     WHERE user_id = $1 AND o.status LIKE 'active'
     GROUP BY p.name, p.price`;
-    const result = await conn.query(getUserOrder, [
-      parseInt(user_id as string),
-    ]);
+    const result = await conn.query(getUserOrder, [parseInt(user_id)]);
     await conn.release();
     return result.rows;
   }
--- src/models/tests/ordersSpec.ts
+++ src/models/tests/ordersSpec.ts
@@ -1,9 +1,10 @@
 import { Order, OrderStore } from "../orders";
 import { Product } from "../products";
-import { OrderProduct } from "../orderProductsController";
+import { OrderProduct } from "../orderProducts";
 import supertest from "supertest";
 import app from "../../server";
 import jwt from "jsonwebtoken";
+import Client from "../../config/database";
 
 const request = supertest(app);
 const store = new OrderStore();
@@ -12,7 +13,7 @@ describe("OrderStore", (): void => {
     expect(store.getUserOrder).toBeDefined();
   });
   it("should get user order", async (): Promise<void> => {
-    const result = await store.getUserOrder(1);
+    const result = await store.getUserOrder("1");
     expect(result).toEqual([] as OrderProduct[]);
   });
 });
@@ -21,11 +22,17 @@ describe("OrderStore", (): void => {
 
 describe("Order endpoints", () => {
   it("Should get all orders", async () => {
-    const token = jwt.sign({ id: 1 }, process.env.JWT_SECRET as string);
+    const conn = await Client.connect();
+    await conn.query(
+      `INSERT INTO users (id, first_name, last_name, password) VALUES (999, 'test', 'test', 'test')`
+    );
+    const token = jwt.sign({ id: 999 }, process.env.JWT_SECRET as string);
     const result = await request
       .get("/api/orders")
       .set({ Authorization: `Bearer ${token}` });
     expect(result.status).toBe(200);
     expect(result.body).toEqual([]);
+    await conn.query(`DELETE FROM users WHERE id = 999`);
+    await conn.release();
   });
 });
