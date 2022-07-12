cc8ec592a5c94a993a1ff484431ac2758aa439d1
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-07-12 19:42:01 +0200

    test: Add endpoint tests to products spec

--- src/models/tests/productsSpec.ts
+++ src/models/tests/productsSpec.ts
@@ -1,5 +1,9 @@
 import { Product, ProductStore } from "../products";
+import app from "../../server";
+import supertest from "supertest";
+import jwt from "jsonwebtoken";
 
+const request = supertest(app);
 const store = new ProductStore();
 
 const product: Product = {
@@ -39,6 +43,48 @@ describe("Product model", () => {
     const result = await store.show("1");
     expect(result).toEqual({
       id: 1,
+      name: "Test",
+      price: 50,
+    });
+  });
+});
+
+// ======================================== PRODUCT ENDPOINTS ========================================
+
+describe("Product endpoints", () => {
+  it("Should get all products", async () => {
+    const result = await request.get("/api/products");
+    expect(result.status).toBe(200);
+    expect(result.body).toEqual([
+      {
+        id: 1,
+        name: "Test",
+        price: 50,
+      },
+    ]);
+  });
+
+  it("Should get a product", async () => {
+    const result = await request.get("/api/products/1");
+    expect(result.status).toBe(200);
+    expect(result.body).toEqual({
+      id: 1,
+      name: "Test",
+      price: 50,
+    });
+  });
+  it("Should create a product", async () => {
+    const token = jwt.sign({ id: 1 }, process.env.JWT_SECRET as string);
+    const result = await request
+      .post("/api/products")
+      .set({
+        "Content-Type": "application/json",
+        authorization: `Bearer ${token}`,
+      })
+      .send(product);
+    expect(result.status).toBe(201);
+    expect(result.body).toEqual({
+      id: 2,
       name: "Test",
       price: 50,
     });
