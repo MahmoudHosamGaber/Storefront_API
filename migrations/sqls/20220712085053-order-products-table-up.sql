fec1a531f94472cdd45c6948d3e91aab79013653
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-07-12 14:29:47 +0200

    fix: Change the password allowed length

--- migrations/sqls/20220712085053-order-products-table-up.sql
+++ migrations/sqls/20220712085053-order-products-table-up.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    password VARCHAR(25)
+    password VARCHAR(255)
 );
 
 CREATE TABLE products (
